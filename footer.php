2c68e09642f33a05d7f6ea9d36dbdf3adf937085
Amsal Karic <amsalk@gmail.com>
2017-01-25 23:01:39 -0500

    About me page supports CMS

--- footer.php
+++ footer.php
@@ -1,15 +1,12 @@
+<footer class="blog-footer">
+  <p>"Look deep into nature, and then you will understand everything better."</p>
+  <p>-Albert Einstein</p>
+  <p><a href="#">Back to top</a></p>
+</footer>
+
 </div><!-- /.container -->
 
     <div class="clearfix"></div>
-
-    <footer class="blog-footer">
-      <p>"Look deep into nature, and then you will understand everything better."</p>
-      <p>-Albert Einstein</p>
-      <p>
-        <a href="#">Back to top</a>
-      </p>
-    </footer>
-
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
--- header.php
+++ header.php
@@ -26,7 +26,7 @@
     <header class="site-header" itemscope="" itemtype="http://schema.org/WPHeader">
       <div class="wrap">
         <div class="site-avatar">
-          <a href="./">
+          <a href="<?php bloginfo('wpurl');?>">
             <img alt="" src="<?php bloginfo('template_directory');?>/images/profile.jpg" srcset="<?php bloginfo('template_directory');?>/images/profile.jpg" class="avatar avatar-224 photo" height="224" width="224">
           </a>
         </div>
@@ -43,8 +43,8 @@
                 <div id="responsive-menu-icon"><i class="fa fa-bars" aria-hidden="true"></i></div>
                 <ul id="menu-top" class="menu nav-menu responsive-menu">
                   <li id="menu-item-119" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-has-children menu-item-119">
-                    <a href="./about-amsal-karic" itemprop="url"><span itemprop="name">About Me</span></a>
-                    <a href="./" itemprop="url"><span itemprop="name">Blog</span></a>
+                    <a href="<?php bloginfo('wpurl');?>/about-amsal-karic" itemprop="url"><span itemprop="name">About Me</span></a>
+                    <a href="<?php bloginfo('wpurl');?>" itemprop="url"><span itemprop="name">Blog</span></a>
                     <!--<ul class="sub-menu">
                       <li id="menu-item-3306" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3306">
                         <a href="#" itemprop="url">
@@ -109,8 +109,3 @@
     </header>
 
     <div class="container">
-
-      <div class="blog-header">
-        <h1 class="blog-title"><?php echo get_bloginfo( 'name' ); ?></h1>
-        <p class="lead blog-description"><?php echo get_bloginfo( 'description' ); ?></p>
-      </div>
