ceea81af097c2b246216dab6c6b9a90720b6df26
Amsal Karic <amsalk@gmail.com>
2017-01-31 23:08:21 -0500

    Form shadow additions & about me changes
    -Added form shadows
    -Added some nice hover transitions
    -Tweaked about me page and added content

--- page-about-amsal-karic.php
+++ page-about-amsal-karic.php
@@ -4,21 +4,11 @@
     <div class="col-sm-12 blog-main" style="padding:40px;">
       <div class="blog-post">
         <h2 class="blog-post-title" style="font-size:35px;">Salutation, World!</h2>
-        <img style="margin:30px 15px 10px 0px; float:left; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);" alt="Brand" width="170px" height="170px" src="<?php bloginfo('template_directory');?>/images/profile.jpg">
-        <p style="margin-top:30px;">I work as a software development manager at grocery ecommerce company <a href="#">MyWebGrocer</a>. Amsal Karic is Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet sollicitudin eros. Aliquam non lorem mauris. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum aliquam pretium sapien condimentum interdum. Quisque efficitur ut mi non ultrices. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at nunc tortor. Sed in odio ultricies, condimentum dolor ut, fringilla velit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at nunc tortor. Sed in odio ultricies, condimentum dolor ut, fringilla velit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at nunc tortor. Sed in odio ultricies, condimentum dolor ut, fringilla velit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at nunc tortor. Sed in odio ultricies, condimentum dolor ut, fringilla velit._</p>
+        <p style="margin-top:30px;">I work as a software development manager at grocery ecommerce company, <a href="http://www.mywebgrocer.com">MyWebGrocer</a>. I have held a number of positions there with my current focus being on analytics and reporting automation. For more info, feel free to take a peek at my resume below.</p>
+        <p>I believe that technology is incredibly beautiful and powerful.</p>
+        <p>More importantly, I believe that it's humanity's only true salvation.</p>
       </div><!-- /.blog-post -->
-    <div id="icon_set" class="center_block" style="display:flex; justify-content:center;">
-        <a href="#"><img src="<?php bloginfo('template_directory');?>/images/icons/gplus.png" style="" width="40px" height="40px"></a>
-        <a href="#"><img src="<?php bloginfo('template_directory');?>/images/icons/linkedin.png" style="margin-left:15px;" width="40px" height="40px"></a>
-        <a href="#"><img src="<?php bloginfo('template_directory');?>/images/icons/mail.png" style="margin-left:15px;" width="40px" height="40px"></a>
-        <a href="#"><img src="<?php bloginfo('template_directory');?>/images/icons/twitter.png" style="margin-left:15px;" width="40px" height="40px"></a>
-        <a href="#"><img src="<?php bloginfo('template_directory');?>/images/icons/instagram.png" style="margin-left:15px;" width="40px" height="40px"></a>
-        <a href="#"><img src="<?php bloginfo('template_directory');?>/images/icons/rss.png" style="margin-left:15px;" width="40px" height="40px"></a>
-    </div>
-    <div id="icon_set" class="center_block" style="display:flex; justify-content:center; margin-top:10px;">
-        <a href="#"><img src="<?php bloginfo('template_directory');?>/images/icons/moar.png" style="" width="40px" height="40px"></a>
-    </div>
-    <a href="<?php bloginfo('template_directory');?>/data/resume_amsal_karic.pdf" style="font-size:30px; margin-top:20px; text-decoration:underline; clear:left;" class="text-center center-block">Resume</a>
+    <a href="<?php bloginfo('template_directory');?>/data/resume_amsal_karic.pdf" style="font-size:30px;" class="text-center center-block">Resume</a>
     </div><!-- /.blog-main -->
   </div><!-- /.row -->
 
